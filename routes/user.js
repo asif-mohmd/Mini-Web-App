4f03f277d08c873b2e76018f2b84e65f28158891
Asif Muhammed <asifasifpsps@gmail.com>
2023-08-22 19:52:45 +0530

    database connected, register and login completed

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,19 @@ var express = require("express");
 var router = express.Router();
 const app = express()
 const session = require("express-session")
+var bodyParser = require('body-parser');
+const { default: mongoose } = require("mongoose");
+
+var jsonParser = bodyParser.json()
+
+mongoose.connect("mongodb://localhost:27017/mini-web-app",{
+    useNewUrlParser: true,
+    useUnifiedTopoLogy:true
+})
+
+var db = mongoose.connection
+db.on("error",()=>console.log("Error in connecting to database"))
+db.once("open",()=>console.log("connection to database"))
 
 // session code
 app.set('trust proxy', 1) // trust first proxy
@@ -15,13 +28,68 @@ app.use(session({
 
 
 router.get("/",(req,res)=>{
-    res.render("user/index")
-})
-
-router.get("/home",(req,res)=>{
     res.render("user/login")
+})
+
+router.get("/index",(req,res)=>{
+    res.render("user/index")
 
 })
+
+router.get("/login",(req,res)=>{
+    res.render("user/login")
+})
+
+router.post("/login", (req,res)=>{
+    console.log(req.body.email)
+    
+    db.collection("users").findOne({email:req.body.email, password:req.body.password}, function(err,user)
+    {
+        console.log(user)
+        console.log("aaima here")
+        if(user) {
+        res.redirect("/index")
+        }else{
+           res.render("user/login")
+        }
+        
+    }
+)});
+
+
+
+router.get("/signup",(req,res)=>{
+    res.render("user/signup")
+
+})
+
+router.post("/signup",(req,res)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    console.log(email)
+    console.log(name)
+    console.log(password)
+
+    var data = {
+        "name" : name,
+        "email" : email,
+        "password": password
+    }
+
+    db.collection("users").insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }else{
+            console.log("recored inserted successfully")
+            res.redirect("/")
+        }
+    })
+    
+})
+
+router.get
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path")
 const hbs = require("hbs")
 const fs = require("fs")
 const userRouter = require("./routes/user")
+const { default: mongoose } = require("mongoose")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
 
@@ -13,14 +16,20 @@ app.set("view engine", "hbs")
 // directory connection
 app.set("views", path.join(__dirname, "views"))
 
+
+mongoose.connect("mongodb://localhost:27017/mini-web-app")
+
+var db = mongoose.connection;
+
+
+
 app.use("/",userRouter)
 
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // req.body convertion with this module otherwise undefined
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+
 
 
 app.listen(PORT,()=>{
