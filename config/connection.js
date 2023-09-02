ffb47853e9b39c048e27c507299af2b3bef59657
Asif Muhammed <asifasifpsps@gmail.com>
2023-09-02 12:45:17 +0530

    db connections edits

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 const { default: mongoose } = require("mongoose");
+
 mongoose.connect("mongodb://localhost:27017/mini-web-app",{
     useNewUrlParser: true,
     useUnifiedTopoLogy:true
 })
 
 var db = mongoose.connection
+
 db.on("error",()=>console.log("Error in connecting to database"))
 db.once("open",()=>console.log("connection to database"))
